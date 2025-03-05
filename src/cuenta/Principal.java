13027fd0916f70d31858efa32db8b803dac3fdb2
jhonbellaizac <janerb123@gmail.com>
2025-03-04 23:18:49 -0500

    Ejemplo 1 uso de git 1

--- src/cuenta/Principal.java
+++ src/cuenta/Principal.java
@@ -2,8 +2,8 @@ package cuenta;
 public class Principal {
     public static void main(String[] args) {
 
-        System.out.println("Hola mundo 232 ");
-        System.out.println("hola mundo2");
+        System.out.println("Hola mundo 55 ");
+       
         Cuenta cuenta= new Cuenta (10000, 0);
         cuenta.imprimir();
         
