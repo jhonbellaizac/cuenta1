8d8c850376a8a5d6c3242c8276f2e25c277aa0ae
jhonbellaizac <janerb123@gmail.com>
2025-03-04 23:31:48 -0500

    Ejemplo 1 uso de git3

--- src/cuenta/Principal.java
+++ src/cuenta/Principal.java
@@ -2,9 +2,9 @@ package cuenta;
 public class Principal {
     public static void main(String[] args) {
 
-        System.out.println("Hola mundo 54 jdhds");
+        System.out.println("Hola");
         System.out.println("hola mundo2");
-       
+        System.out.println("hola");
         Cuenta cuenta= new Cuenta (10000, 0);
         cuenta.imprimir();
         
