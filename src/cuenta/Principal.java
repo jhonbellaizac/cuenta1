2b21315d9b3b0c40a1c136d1dacb65cf2ab97643
jhonbellaizac <janerb123@gmail.com>
2025-03-04 22:48:07 -0500

    Ejemplo uso de git

---
+++ src/cuenta/Principal.java
@@ -0,0 +1,26 @@
+package cuenta;
+public class Principal {
+    public static void main(String[] args) {
+
+        System.out.println("Hola mundo");
+        Cuenta cuenta= new Cuenta (10000, 0);
+        cuenta.imprimir();
+        
+        cuenta.consignar(5245);
+        cuenta.imprimir();
+        
+        cuenta.retirar(1501);
+        cuenta.imprimir();
+        
+    }
+    
+}
+
+
+
+
+
+
+    
+    
+
