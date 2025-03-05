1cbdf9808360a02cd14ed50a15d0ee0584ac5db1
jhonbellaizac <janerb123@gmail.com>
2025-03-05 13:23:21 -0500

    quitar el hola mundo1

--- src/cuenta/Principal.java
+++ src/cuenta/Principal.java
@@ -2,9 +2,7 @@ package cuenta;
 public class Principal {
     public static void main(String[] args) {
 
-        System.out.println("Hola");
-        System.out.println("hola mundo2");
-        System.out.println("hola");
+      
         Cuenta cuenta= new Cuenta (10000, 0);
         cuenta.imprimir();
         
