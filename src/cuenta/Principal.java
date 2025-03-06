666b5f9e8464e3f5e510c7388c48ad5b81f4c434
jhonbellaizac <janerb123@gmail.com>
2025-03-05 21:06:31 -0500

    aumente el valor

--- src/cuenta/Principal.java
+++ src/cuenta/Principal.java
@@ -3,21 +3,20 @@ public class Principal {
     public static void main(String[] args) {
 
       
-        Cuenta cuenta= new Cuenta (10000, 0);
+        Cuenta cuenta= new Cuenta (1000000, 0);
         cuenta.imprimir();
         System.out.println("");
         
-        cuenta.consignar(5245);
+        cuenta.consignar(52450);
         cuenta.imprimir();
         System.out.println("");
         
-        cuenta.retirar(1501);
+        cuenta.retirar(25200);
         cuenta.imprimir();
         System.out.println("");
         
-        cuenta.interesMensual();
-        cuenta.imprimir();
-        System.out.println("");
+       
+        
     }
     
 }
